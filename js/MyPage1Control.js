fba27aeb0eda1f5862d3fcec1e8750e6554a327a
paytrenindramayu <46125812+paytrenindramayu@users.noreply.github.com>
2018-12-31 15:27:41 +0700

    Update MyPage1Control.js

--- js/MyPage1Control.js
+++ js/MyPage1Control.js
@@ -43,19 +43,9 @@ $(() => {
     pagCtl.MyStickyNavbar("myskickynavbar1");
 });
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("myBtn").style.display = "block";
-  } else {
-    document.getElementById("myBtn").style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+//browser window scroll (in pixels) after which the "back to top" link is shown
+var offset = 300, 
+    //browser window scroll (in pixels) after which the "back to top" link opacity is reduced 
+    offsetOpacity = 1200,
+    //duration of the top scrolling animation (in ms)
+    scrollDuration = 700;
