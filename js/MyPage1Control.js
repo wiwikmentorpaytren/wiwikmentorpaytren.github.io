27402f2557f9651540cba9716204124714b3fe4d
paytrenindramayu <46125812+paytrenindramayu@users.noreply.github.com>
2018-12-31 15:39:03 +0700

    Update MyPage1Control.js

--- js/MyPage1Control.js
+++ js/MyPage1Control.js
@@ -42,10 +42,3 @@ $(() => {
     // If the page is already scrolled down more than the nave bar, it will set
     pagCtl.MyStickyNavbar("myskickynavbar1");
 });
-
-//browser window scroll (in pixels) after which the "back to top" link is shown
-var offset = 300, 
-    //browser window scroll (in pixels) after which the "back to top" link opacity is reduced 
-    offsetOpacity = 1200,
-    //duration of the top scrolling animation (in ms)
-    scrollDuration = 700;
